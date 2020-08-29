80f1795e9da814a78dd0203764c38dd0bb0d0d3f
zhang <>
2020-08-29 12:09:20 -0400

    add user namagement

---
+++ wwwroot/js/common.js
@@ -0,0 +1,68 @@
+﻿$.ajaxSetup({
+    crossDomain: true,
+    xhrFields: {
+        withCredentials: true
+    }
+});
+
+function get_root() {
+
+    //return "http://localhost:8080/";
+    return "https://asharea-viewer.azurewebsites.net/";
+}
+
+
+function get_root_Api() {
+
+    return "http://localhost:8786/";
+    //return "https://asharea-api.azurewebsites.net/";
+}
+
+function pop_info_go(title, text, url) {
+    Swal.fire({
+        title: title,
+        text: text
+    }).then((result) => {
+        window.location.href = url;
+    })
+}
+function pop_info(title, text) {
+    Swal.fire({
+        title: title,
+        text: text
+    });
+}
+
+
+function get_user_info(userId) {
+    return $.ajax({
+        type: 'GET',
+        url: get_root_Api() + 'api/GetUserInfo',
+        data: {
+            "userId": userId
+        },
+        success: function (data) {
+        },
+        error: function (error) {
+            process_error(error);
+        }
+    });
+}
+
+function process_error(data) {
+    console.log(data);
+    if (data['status'] === 401) {
+        window.location.href = "./login.html" + "?redirect=" + encodeURIComponent(window.location.href);
+    } else{
+        pop_info("Error", data['responseJSON']['message']);
+    }
+}
+
+function logout() {
+    return $.ajax({
+        type: 'POST',
+        url: get_root_Api() + 'Logout',
+        success: function (data) {
+        }
+    });
+}
---
+++ wwwroot/js/dropzone.js
@@ -0,0 +1,92 @@
+﻿$(function () {
+    Dropzone.autoDiscover = false;
+});
+
+function init_dropzone(ele_id, is_multi, drop_callback) {
+    console.log(ele_id);
+    let dropZone = document.getElementById(ele_id);
+
+    if (!dropZone.dropzone) {
+        new Dropzone("#" + ele_id);
+    }
+
+    dropZone.dropzone.options.addRemoveLinks = true;
+    dropZone.dropzone.options.maxFilesize = 500;
+    dropZone.dropzone.options.autoProcessQueue = false;
+    dropZone.dropzone.on("error", function (file) {
+        if (!file.accepted) this.removeFile(file);
+    });
+
+    function showDropZone() {
+        dropZone.style.display = "block";
+    }
+
+    function hideDropZone() {
+        dropZone.style.display = "none";
+    }
+
+    function allowDrag(e) {
+        if (e) {
+            e.preventDefault();
+        }
+    }
+
+    function handel_new_file(add_files) {
+        let files = dropZone.dropzone.files;
+        let add = files.length > 0 && files[files.length - 1].name === add_files[add_files.length - 1].name;
+        clear_dz_files(files);
+
+        let file = add_files ? add_files[0] : files[files.length - 1];
+        if (drop_callback) {
+            drop_callback(file);
+        } else if (add) {
+            dropZone.dropzone.addFile(file);
+        }
+    }
+
+    function clear_dz_files(files) {
+        if (!files) {
+            files = dropZone.dropzone.files;
+        }
+        if (files && files.length > 0) {
+            for (let i = 0; i < files.length; i++) {
+                dropZone.dropzone.removeFile(files[i]);
+            }
+        }
+    }
+
+    function handleDrop(e) {
+        e.preventDefault();
+        if (drop_callback) {
+            clear_dz_files();
+        } else if (!is_multi) {
+            let files = dropZone.dropzone.files;
+            if (files && files.length > 0) {
+                for (let i = 1; i < files.length; i++) {
+                    dropZone.dropzone.removeFile(files[i]);
+                }
+            }
+        }
+    }
+
+    window.addEventListener('dragenter', function (e) {
+        showDropZone();
+    });
+    dropZone.addEventListener('dragenter', allowDrag);
+    dropZone.addEventListener('dragover', allowDrag);
+    dropZone.addEventListener('dragleave', function (e) {
+        // hideDropZone();
+    });
+    dropZone.addEventListener('drop', handleDrop);
+    dropZone.dropzone.on('addedfiles', function (files) {
+        let dz_files = dropZone.dropzone.files;
+        if (dz_files && dz_files.length === 0 && !files) {
+            pop_info("", "Please drop a file");
+            return;
+        }
+
+        if (!is_multi) {
+            handel_new_file(files);
+        }
+    });
+}
