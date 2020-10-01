2a80a036e1a0512ecc22e3ae0bd63c42b21c4eb2
zhang <>
2020-10-01 01:02:30 -0400

    connect  with DOEgbxml project

---
+++ Data/model/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Asharea_viewer.Data.model
+{
+    public class Info
+    {
+        public string key { get; set; }
+        public string content { get; set; }
+
+
+        public Info(string key, string content) {
+            this.key = key;
+            this.content = content;
+
+        }
+    }
+}
---
+++ Data/model/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Asharea_viewer.Data.model
+{
+    public class Test
+    {
+        public string title { get; set; }
+        public bool isPassed { get; set; }
+        public string result { get; set; }
+        public Info[] infos { get; set; }
+        public Test(string title, bool isPassed, string result) {
+            this.title = title;
+            this.isPassed = isPassed;
+            this.result = result;
+        }
+    }
+    
+}
